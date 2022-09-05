df96c4cff031064a4348e213e74f69b706a40b8e
DalerKulchukov <laxusdreyar20020709@gmail.com>
2022-09-05 22:26:50 +0600

    dz1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
+
+
 import './App.css';
+
+function AboutUs () {
+  return(
+    <>
+      <h1>title</h1>
+      <p>text</p>
+    </>
+  );
+};
+
+function Portfolio () {
+  return(
+    <ul>
+      <li>test1</li>
+    </ul>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AboutUs/>
+      <Portfolio/>
     </div>
   );
 }
